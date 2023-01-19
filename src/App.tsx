2e74b0fda03552b0822d5886364b365e7e01bdad
dpatanin <da-patanin@t-online.de>
2023-01-19 14:51:00 +0100

    create base homepage
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Icon, Link } from '@chakra-ui/react';
+import { VscGithub } from 'react-icons/vsc';
 import './App.css';
 import Main from './Main';
 
 function App() {
   return (
     <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
-          <Main />
-        </header>
-      </div>
+      <div className="bg"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
+      <Link href="https://github.com/dpatanin/WastedOnLearning" isExternal>
+        <Icon
+          as={VscGithub}
+          pos="absolute"
+          top="2.5"
+          right="2.5"
+          boxSize="8"
+          color="white"
+        />
+      </Link>
+      <Main />
     </ChakraProvider>
   );
 }
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,27 @@
+import { Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+
 export default function Main() {
-    return (
-        <div>
-            /(^_^)\
-        </div>
-    )
+  return (
+    <Container maxW="4xl">
+      <Stack
+        textAlign={'center'}
+        spacing={{ base: 6, md: 8 }}
+        py={{ base: 14, md: 24 }}
+      >
+        <Image src="/icon.svg" boxSize="120px" alignSelf="center" />
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+          color="white"
+        >
+          WastedOnLearning
+        </Heading>
+        <Text color={'white'} fontSize="xl">
+          Estimate the time required to complete an eLearning or a set of
+          courses. Upload the file(s), define the content & target audience and
+          receive an estimation how long it would take them.
+        </Text>
+      </Stack>
+    </Container>
+  );
 }
