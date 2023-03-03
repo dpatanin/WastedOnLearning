3d21467b9ec969b59809972eaa11cfe18b2aa42a
dpatanin <da-patanin@t-online.de>
2023-03-03 11:53:32 +0100

    analyze pdf file WIP
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- src/calculator/PDF.ts
+++ src/calculator/PDF.ts
@@ -1,13 +1,46 @@
+import pdfjs from 'pdfjs-dist';
 import { countWords } from '../lib/helper';
 import { FileCalData } from './controller';
 
-export default function PDF(file: File): Promise<FileCalData> {
-  return file.text().then((text) => {
-    return {
-      wordCount: countWords(text),
-      imageCount: 0,
-      videoLengthSec: 0,
-      audioLengthSec: 0,
-    };
+export default async function PDF(file: File): Promise<FileCalData> {
+  const arrayBuffer = await file.arrayBuffer();
+  return pdfjs.getDocument(arrayBuffer).promise.then(async (pdf) => {
+    const texts: string[] = [];
+    const images = [];
+
+    const pagesPromises: Promise<any>[] = [];
+
+    for (let i = 1; i <= pdf.numPages; i++) {
+      pagesPromises.push(
+        pdf.getPage(i).then((proxy) => {
+          proxy.getTextContent().then((content) => {
+            content.items.forEach((item) => {
+              if ('str' in item) texts.push(item.str);
+            });
+          });
+
+          proxy.getOperatorList().then((opList) => {
+            opList.fnArray.forEach((element) => {
+              if (
+                [
+                  pdfjs.OPS.paintImageXObject, // 85
+                  pdfjs.OPS.paintImageXObjectRepeat, // 88
+                ].includes(element)
+              )
+                images.push(element);
+            });
+          });
+        })
+      );
+    }
+
+    return Promise.all(pagesPromises).then(() => {
+      return {
+        wordCount: countWords(texts.join('')),
+        imageCount: images.length,
+        videoLengthSec: 0,
+        audioLengthSec: 0,
+      };
+    });
   });
 }
